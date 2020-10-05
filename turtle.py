4e798f38e51329bc739d4e4b9dc8d3673bf4da9d
unknown <ynarula678@gmail.com>
2020-10-05 16:07:49 +0530

    draw fixes

--- turtle.py
+++ turtle.py
@@ -4,3 +4,10 @@ wn = turtle.Screen()
 wn.bgcolor("black")
 #colors
 colors = ["violet", "indigo", "blue","green","yellow","orange","red"]
+draw = turtle.Turtle()
+for x in range(360):
+    draw.pencolor(colors[x % 7])
+    draw.width(x/100 + 1)
+    draw.forward(x)
+    draw.left(59)
+turtle.done()
